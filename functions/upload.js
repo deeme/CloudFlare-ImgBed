6ce42dce7119bfe5b7d9bfa08cc4ad59fab76a3c
deeme <xyuant@gmail.com>
2024-12-18 00:38:17 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -235,7 +235,7 @@ async function uploadFileToCloudflareR2(env, formdata, fullId, metadata) {
         JSON.stringify([{ 'src': `/file/${fullId}` }]), 
         {
             status: 200,
-            headers: { 'Content-Type': 'application/json' }
+            headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
         }
     );
 }
